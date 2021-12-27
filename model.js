61793587767c9ec99515be7552318a6e3453de9a
Andy Monis <andy.monis@anthropos.io>
2021-12-27 21:37:58 +0000

    Checking in latest

--- model.js
+++ model.js
@@ -10,21 +10,25 @@ let Model = {
 
     // Methods
     home_on_app_statistics: async function() {
-        // Post results to authorize
-        let res = await V3Store.$post("/services/ops/default/statistics", { app: "notes" });
-        let records = res.records;
+        // Get all Apps
+        let results = await V3Store.$get("/services/ops/apps");
+        let apps = results.records;
+
+        // Iterate over all apps
         let out = [];
         out.push(`<div class="min-w-full divide-y divide-gray-200">`);
-
-        for (let i = 0; i < records.length; i++) {
-            out.push(`<div>`);
-            out.push(`<span class="px-6 py-4 whitespace-nowrap">${records[i].ts_readable}</span>`);
-            out.push(`<span class="px-6 py-4 whitespace-nowrap">${records[i].app_id}</span>`);
-            out.push(`<span class="px-6 py-4 whitespace-nowrap">${records[i].web_id}</span>`);
-            out.push(`<span class="px-6 py-4 whitespace-nowrap">${records[i].action}</span>`);
-            out.push(`</div>`);
+        for (let j = 0; j < apps.length; j++) {
+            // Post results to authorize
+            let app = await V3Store.$get(`/services/ops/${apps[j].id}/statistics`);
+            for (let i = 0; i < app.records.length; i++) {
+                out.push(`<div>`);
+                out.push(`<span class="px-6 py-4 whitespace-nowrap">${app.records[i].ts_readable}</span>`);
+                out.push(`<span class="px-6 py-4 whitespace-nowrap">${app.records[i].app_id}</span>`);
+                out.push(`<span class="px-6 py-4 whitespace-nowrap">${app.records[i].web_id}</span>`);
+                out.push(`<span class="px-6 py-4 whitespace-nowrap">${app.records[i].action}</span>`);
+                out.push(`</div>`);
+            }
         }
-
         out.push(`</div>`);
 
         this.console(out.join(""));
